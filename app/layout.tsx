c528c686fca1685b19dec6c9e8c8bed9ac85a4d8
Rafi <mrkamin2@gmail.com>
2025-05-03 11:08:06 +0500

    Build Header and Footer Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
+import type { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "../styles/globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+type RootLayoutProps = {
+  children: ReactNode;
+}
+const outfit = Outfit({
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
@@ -17,17 +18,17 @@ export const metadata: Metadata = {
   description: "Bagella is an ecommerce platform for saling company products",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.className} antialiased flex flex-col min-h-screen`}
       >
-        {children}
+        <Header />
+        <main className="max-w-7xl my-8 mx-auto px-4 flex-grow w-full">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
