664a43c9c41c489ed032427680cb25a38efeb594
Rafi <mrkamin2@gmail.com>
2025-05-03 07:37:34 +0500

    Customize shadcn ui theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
-    <div>
-      Bagella
-    </div>
+    <Button>Bagella</Button>
   )
 }
 
