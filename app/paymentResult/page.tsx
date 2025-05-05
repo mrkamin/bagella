ee23f81db00543c0205d0cdc8eaff73991862295
Rafi <mrkamin2@gmail.com>
2025-05-05 11:20:43 +0500

    Fix Type Error

--- app/paymentResult/page.tsx
+++ app/paymentResult/page.tsx
@@ -1,58 +1,60 @@
-import { stripe } from "@/lib/Stripe"
+import { stripe } from "@/lib/Stripe";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import TickIcon from "@/components/ui/Tickicon";
 
-interface PageProps  {
-  searchParams: {session_id?: string};
+interface PageProps {
+  searchParams?: { [key: string]: string | string[] | undefined };
 }
 
 async function getSession(sessionId: string) {
   try {
-    const session = await stripe.checkout.sessions.retrieve
-    (sessionId);
-    return session
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+    return session;
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return null;
   }
 }
 
-const page = async ({searchParams}: PageProps) => {
-  const sessionId =  searchParams.session_id;
-  if(!sessionId) {
-    redirect("/")
+const Page = async ({ searchParams }: PageProps) => {
+  const sessionId = searchParams?.session_id;
+
+  if (!sessionId || Array.isArray(sessionId)) {
+    redirect("/");
   }
-  const session = await getSession(sessionId)
+
+  const session = await getSession(sessionId);
+
   if (!session) {
-    return <h1>Invalid session!</h1>
+    return <h1>Invalid session!</h1>;
   }
+
   if (session?.status === "expired") {
-    return <h1>Your session expired</h1>
+    return <h1>Your session expired</h1>;
   }
+
   if (session?.status === "open") {
-    return <h1>Your payment is in Progress!</h1>
+    return <h1>Your payment is in Progress!</h1>;
   }
+
   return (
     <div className="flex flex-col gap-4 items-center">
-      <div className="w-12 h-12">
+      <div className="w-12 h-12 text-green-600">
         <TickIcon />
       </div>
-      <h1 className="text-2xl font-semibold">
-        Payment Successful!
-      </h1>
+      <h1 className="text-2xl font-semibold">Payment Successful!</h1>
       <p className="text-lg max-w-md text-center">
-        Hello {session.customer_details?.name}, Your order has been successfully
-        placed! Expect delivery within 2-3 business days. Purchase
-        Receipt is sent to your email: {session.customer_details?.email}
+        Hello {session.customer_details?.name}, your order has been successfully
+        placed! Expect delivery within 2-3 business days. A receipt has been
+        sent to: {session.customer_details?.email}
       </p>
-      <Link href='/'>
+      <Link href="/">
         <Button>Go Home</Button>
       </Link>
-      
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
